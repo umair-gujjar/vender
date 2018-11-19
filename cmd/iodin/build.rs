c6286ecbb97b7fffec9d2ef40cfb1e2d0eaafa6d
Sergey Shepelev <temotor@gmail.com>
2018-11-19 12:48:50 +0500

    kitchen devices initial support + many related

--- cmd/iodin/build.rs
+++ cmd/iodin/build.rs
@@ -3,12 +3,15 @@ extern crate protoc_rust;
 use protoc_rust::Customize;
 
 fn main() {
+    let input = "../../protobuf/iodin.proto";
+    println!("rerun-if-changed={}", input);
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/proto",
-        input: &["../../protobuf/iodin.proto"],
+        input: &[input],
         includes: &["../../protobuf"],
         customize: Customize {
             ..Default::default()
         },
-    }).expect("protoc");
+    })
+    .expect("protoc");
 }
